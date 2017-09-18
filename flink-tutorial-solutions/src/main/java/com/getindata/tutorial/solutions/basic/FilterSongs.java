40402cce2be9c1fb2f9d9b01f41afc4060979f9e
Dawid Wysakowicz <dawid@getindata.com>
2017-09-18 12:51:46 +0200

    task2

--- flink-tutorial-solutions/src/main/java/com/getindata/tutorial/solutions/basic/FilterSongs.java
+++ flink-tutorial-solutions/src/main/java/com/getindata/tutorial/solutions/basic/FilterSongs.java
@@ -1,26 +1,26 @@
-package com.getindata.tutorial.solutions.basic;
-
-import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-
-import com.getindata.tutorial.base.input.SongsSource;
-import com.getindata.tutorial.base.model.SongEvent;
-
-public class FilterSongs {
-	public static void main(String[] args) throws Exception {
-		final StreamExecutionEnvironment sEnv = StreamExecutionEnvironment.getExecutionEnvironment();
-
-		// create a stream of events from source
-		final DataStream<SongEvent> events = sEnv.addSource(new SongsSource());
-
-		// filter events
-		final DataStream<SongEvent> filteredEvents = events.filter(/* INSERT YOUR CODE HERE */);
-
-		// print filtered events
-		filteredEvents.print();
-
-		// execute streams
-		sEnv.execute();
-	}
-}
+//package com.getindata.tutorial.solutions.basic;
+//
+//import org.apache.flink.api.common.functions.FilterFunction;
+//import org.apache.flink.streaming.api.datastream.DataStream;
+//import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+//
+//import com.getindata.tutorial.base.input.SongsSource;
+//import com.getindata.tutorial.base.model.SongEvent;
+//
+//public class FilterSongs {
+//	public static void main(String[] args) throws Exception {
+//		final StreamExecutionEnvironment sEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+//
+//		// create a stream of events from source
+//		final DataStream<SongEvent> events = sEnv.addSource(new SongsSource());
+//
+//		// filter events
+//		final DataStream<SongEvent> filteredEvents = events.filter(/* INSERT YOUR CODE HERE */);
+//
+//		// print filtered events
+//		filteredEvents.print();
+//
+//		// execute streams
+//		sEnv.execute();
+//	}
+//}
