3c88b779ba0b4072a50bb57a21f944ec524dd344
Grzegorz Kołakowski <grzegorz.kolakowski@getindata.com>
2020-02-25 17:46:45 +0100

    Remove flink-tutorial-solutions module

--- flink-tutorial-base/src/main/java/com/getindata/tutorial/base/kafka/KafkaProperties.java
+++ flink-tutorial-base/src/main/java/com/getindata/tutorial/base/kafka/KafkaProperties.java
@@ -24,8 +24,8 @@ public class KafkaProperties {
 
     public static String getUsername() {
         // FIXME please return your username
-         return "lion";
-//        throw new UnsupportedOperationException("Please provide your user name.");
+//         return "lion";
+        throw new UnsupportedOperationException("Please provide your user name.");
     }
 
     public static Properties getKafkaProperties() {
@@ -34,9 +34,9 @@ public class KafkaProperties {
         // properties.setProperty("bootstrap.servers", "kafka:9092");
         // return properties;
         // FIXME: uncomment if you are going to use yarn cluster
-//         properties.setProperty("bootstrap.servers", "flink-slave-01.c.getindata-training.internal:9092,flink-slave-02.c.getindata-training.internal:9092,flink-slave-03.c.getindata-training.internal:9092,flink-slave-04.c.getindata-training.internal:9092,flink-slave-05.c.getindata-training.internal:9092");
-//         return properties;
-        throw new UnsupportedOperationException("Please provide Kafka bootstrap servers.");
+         properties.setProperty("bootstrap.servers", "flink-slave-01.c.getindata-training.internal:9092,flink-slave-02.c.getindata-training.internal:9092,flink-slave-03.c.getindata-training.internal:9092,flink-slave-04.c.getindata-training.internal:9092,flink-slave-05.c.getindata-training.internal:9092");
+         return properties;
+//        throw new UnsupportedOperationException("Please provide Kafka bootstrap servers.");
     }
 
     public static String getTopic(String user) {
