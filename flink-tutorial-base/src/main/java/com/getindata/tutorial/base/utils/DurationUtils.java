b6434c111afa71434a03bbcaebd3c54599edc148
Adrian Bednarz <adrianbednarz@gmail.com>
2021-02-22 11:43:06 +0100

    Fixes

--- flink-tutorial-base/src/main/java/com/getindata/tutorial/base/utils/DurationUtils.java
+++ flink-tutorial-base/src/main/java/com/getindata/tutorial/base/utils/DurationUtils.java
@@ -3,15 +3,15 @@ package com.getindata.tutorial.base.utils;
 
 public class DurationUtils {
 
-    public static String formatDuration(long seconds) {
-        long absSeconds = Math.abs(seconds);
+    public static String formatDuration(long millis) {
+        long absSeconds = Math.abs(millis) / 1000;
         String positive = String.format(
                 "%d:%02d:%02d",
                 absSeconds / 3600,
                 (absSeconds % 3600) / 60,
                 absSeconds % 60
         );
-        return seconds < 0 ? "-" + positive : positive;
+        return millis < 0 ? "-" + positive : positive;
     }
 
     private DurationUtils() {
